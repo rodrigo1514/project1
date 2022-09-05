e7ab35760bf5960430680cb550dc81c938391367
rodri <rodri@LAPTOP-IHKG3KLI.myfiosgateway.com>
2022-09-04 21:34:22 -0400

    Test 2

---
+++ CountByFives/src/CountByFives.java
@@ -0,0 +1,24 @@
+
+public class CountByFives {
+
+	public static void main(String[] args) {
+		{
+			
+			{
+			final int START = 5;
+			final int STOP = 500;
+			final int NUMBER_PER_LINE = 50;
+			for(int i = START; i <= STOP; i += START)
+			{
+			System.out.print(i + " ");
+			if(i % NUMBER_PER_LINE == 0)
+			System.out.println();
+			}
+			
+			
+
+	}
+
+}
+}
+}
